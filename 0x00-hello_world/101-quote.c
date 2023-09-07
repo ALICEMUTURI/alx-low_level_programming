c433d4cb7236eb640bbf40275451a7facadd0a28
ALICEMUTURI <alicemuturi2003@gmail.com>
2023-09-07 12:37:17 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Outputs a specified string to stderr
+ * Return: Returns 1 on success
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\"");
+fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+return (1);
+}
