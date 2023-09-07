e43f24f3919e90c437daa88b5ac4ad150ac70f02
ALICEMUTURI <alicemuturi2003@gmail.com>
2023-09-07 12:10:55 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Outputs a specific string to stdout
+ * Return: Returns 0 on success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Outputs a specific string to stdout
+ * Return: Returns 0 on success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
